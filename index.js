22dfe60e929b2d61a00c106cc502523f5e36ea73
oarano <oarano@MacBook-Pro-de-oarano-2.local>
2020-07-31 13:36:40 +0200

    Player finished - JS

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
-const cover = document.querySelector('.card-image');
+const image = document.querySelector('.card-image');
 const titre = document.querySelector('.card-content h5');
 const artiste = document.querySelector('.artiste');
 const audio = document.querySelector('.audio')
+const categorie = document.querySelector('.categorie')
 
 const data = {
     titre: "",
+    categorie: "ROCK",
     artiste: "Motley Crue",
-    cover: "https://39s.musify.club/img/70/6482339/19023231.jpg",
-    file: ""
+    image: "https://39s.musify.club/img/70/6482339/19023231.jpg",
+    file: "files/file1.mp3"
 }
+image.style.background = `url('${data.image}') no-repeat center center / cover`;
+audio.src = data.file;
+categorie.innerText = data.categorie
