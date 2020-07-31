a6be1b66cffcc6446739d6dc8b606fd72fa21f42
oarano <oarano@MacBook-Pro-de-oarano-2.local>
2020-07-31 13:11:40 +0200

    Player HTML / CSS finished

---
+++ index.js
@@ -0,0 +1,11 @@
+const cover = document.querySelector('.card-image');
+const titre = document.querySelector('.card-content h5');
+const artiste = document.querySelector('.artiste');
+const audio = document.querySelector('.audio')
+
+const data = {
+    titre: "",
+    artiste: "Motley Crue",
+    cover: "https://39s.musify.club/img/70/6482339/19023231.jpg",
+    file: ""
+}
